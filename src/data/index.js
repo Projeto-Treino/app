5564836a4188e3dbed12ce3b78b122020f67fcbd
Felipe Fahrion <felipefahrion@POATEKMBP-Felipe-Fahrion.local>
2024-04-12 11:09:50 -0300

    add new user

--- src/data/index.js
+++ src/data/index.js
@@ -207,4 +207,212 @@ export const usersTreios = {
             },
         ],
     },
+    "84909269053": {
+        treinos: [
+            {
+                treino: "A",
+                exercicios: [
+                    {
+                        "exercicio": "REMADA BAIXA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [40]
+                    },
+                    {
+                        "exercicio": "SERROTE",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [12]
+                    },
+                    {
+                        "exercicio": "PUXADA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [45]
+                    },
+                    {
+                        "exercicio": "ROSCA DIRETA E ALTERNADA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [7]
+                    },
+                    {
+                        "exercicio": "ROSCA SCOTT",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [7]
+                    },
+                    {
+                        "exercicio": "ABDOMINAL REMADOR",
+                        "series": 4,
+                        "repeticoes": "30",
+                        "carga": []
+                    }
+                ]
+                ,
+            },
+            {
+                treino: "B",
+                exercicios: [
+                    {
+                        "exercicio": "LEG PRESS",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [50]
+                    },
+                    {
+                        "exercicio": "SQUAT MACHINE",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [20]
+                    },
+                    {
+                        "exercicio": "AGACHAMENTO",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": []
+                    },
+                    {
+                        "exercicio": "TERRA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [10]
+                    },
+                    {
+                        "exercicio": "MESA FLEXORA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [20]
+                    }
+                ]
+
+            },
+            {
+                treino: "C",
+                exercicios: [
+                    {
+                        "exercicio": "SUPINO RETO",
+                        "equipamento": "HALTERES",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [20]
+                    },
+                    {
+                        "exercicio": "CRUCIFIXO INCLINADO",
+                        "equipamento": "BANCO 30°",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [7]
+                    },
+                    {
+                        "exercicio": "CRUCIFIXO NA MÁQUINA",
+                        "observacao": "2 SEGUNDOS NO PICO DE CONTRAÇÃO",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [40]
+                    },
+                    {
+                        "exercicio": "DESENVOLVIMENTO",
+                        "equipamento": "NA MÁQUINA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [7]
+                    },
+                    {
+                        "exercicio": "ELEVAÇÃO LATERAL",
+                        "equipamento": "NO CROSS / POLIA NA ALTURA DO QUADRIL",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [6]
+                    },
+                    {
+                        "exercicio": "ELEVAÇÃO FRONTAL",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [35]
+                    }
+                ]
+                
+            },
+            {
+                treino: "D",
+                exercicios: [
+                    {
+                        "exercicio": "TERRA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [10]
+                    },
+                    {
+                        "exercicio": "PASSADA COM HALTERES",
+                        "series": 4,
+                        "repeticoes": "30 PASSOS",
+                        "carga": [20]
+                    },
+                    {
+                        "exercicio": "CADEIRA FLEXORA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [16, 50]
+                    },
+                    {
+                        "exercicio": "LEG PRESS",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [90]
+                    }
+                ]
+                
+            },
+            {
+                treino: "E",
+                exercicios: [
+                    {
+                        "exercicio": "TRÍCEPS CROSS",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [40]
+                    },
+                    {
+                        "exercicio": "TRÍCEPS TESTA COM HALTERES",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [16]
+                    },
+                    {
+                        "exercicio": "TRÍCEPS BANCO PERNAS ESTENDIDAS",
+                        "series": 4,
+                        "repeticoes": "ATÉ A FALHA",
+                        "carga": []
+                    },
+                    {
+                        "exercicio": "ROSCA DIRETA E ALTERNADA",
+                        "observacao": "ALTERNADA",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [8]
+                    },
+                    {
+                        "exercicio": "ROSCA SCOTT",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [8]
+                    },
+                    {
+                        "exercicio": "MARTELO",
+                        "series": 4,
+                        "repeticoes": "10-12",
+                        "carga": [8]
+                    },
+                    {
+                        "exercicio": "ABDOMINAL REMADOR",
+                        "series": 4,
+                        "repeticoes": "20",
+                        "carga": []
+                    }
+                ]
+                
+            },
+        ],
+    },
 };
