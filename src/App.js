3410baf71ba92a579527cf773bd1284518663c61
Felipe Fahrion <felipefahrion@POATEKMBP-Felipe-Fahrion.local>
2024-04-12 11:03:02 -0300

    update data and train visualization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { usersTreios } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [cpf, setCpf] = useState("");
+  const [treinos, setTreinos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Buscando treino");
+
+    console.log(cpf);
+
+    const treinos = usersTreios[cpf].treinos;
+    setTreinos(treinos);
+    console.log(treinos);
+  };
+
+  const handleChangeInput = (event) => {
+    console.log(event.target.value);
+    setCpf(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ficha de treino</h1>
+
+      <form>
+        <input
+          type="text"
+          value={cpf}
+          onChange={handleChangeInput}
+          placeholder="Digite seu CPF"
+        />
+        <button type="submit" onClick={(e) => handleSubmit(e)}>
+          Buscar
+        </button>
+      </form>
+
+      <div id="treinos">
+        {treinos.map((treino, index) => (
+          <div key={index}>
+            <h2>Treino {treino.treino}</h2>
+            <div className="cardsContainer">
+            {treino.exercicios.map((exercicio, index) => (
+              <div className="cards" key={index}>
+                <h3>Exercício: {exercicio.exercicio}</h3>
+                <p>Series: {exercicio.series}</p>
+                <p>Repetições: {exercicio.repeticoes}</p>
+                <p>Carga: {exercicio.carga}</p>
+                <a href={exercicio.link}>Link</a>
+              </div>
+            ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
